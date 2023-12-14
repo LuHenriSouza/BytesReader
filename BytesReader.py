07734eaafd334120aec88b5176b3697d50d5671a
luizhapsouza <luizhapsouza@gmail.com>
2023-12-14 14:16:03 -0300

    changed the data output

--- BytesReader.py
+++ BytesReader.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 def ler_primeiros_bytes(arquivo, num_bytes):
     with open(arquivo, 'rb') as f:
@@ -15,9 +16,15 @@ def main():
     arquivo = args.arquivo
     num_bytes = args.num_bytes
 
+    path = os.path.join(os.path.expanduser('~'), 'Desktop', 'DdnetFristBytes')
+    
     try:
         primeiros_bytes = ler_primeiros_bytes(arquivo, num_bytes)
-        print(f'Primeiros {num_bytes} bytes do arquivo "{arquivo}": {primeiros_bytes}')
+        with open(path, 'wb') as arquivo:
+            arquivo.write(primeiros_bytes)
+        
+        print(f"Criado arquivo em {path}")
+
     except Exception as e:
         print(f'Erro ao ler os bytes do arquivo: {e}')
 
